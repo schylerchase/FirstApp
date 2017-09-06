fe3a7da8878681e66c7c5ebd8af2be60b4903627
schylerchase <ryan.schyler@gmail.com>
2017-09-05 23:09:33 -0400

    First Commit

---
+++ App.java
@@ -0,0 +1,8 @@
+public class App
+{
+  public static void main(String[] args)
+  {
+    System.out.println( "My First App" );
+    System.out.println( "This is my first one");
+  }
+}
---
+++ GuiApp.java
@@ -0,0 +1,14 @@
+// Filename GuiApp.java
+// Written by Schyler Ryan
+// Written on 9/5/2017
+
+import javax.swing.JOptionPane;
+/*
+ * This program uses a GUI Object */
+public class GuiApp
+{
+  public static void main( String[] args)
+  {
+   JOptionPane.showMessageDialog( null, "My first Java app");
+  }
+}
